6341e68c7d7f577e5925144c6d5baee66a0aa181
SSKHAN9 <122330321+SSKHAN9@users.noreply.github.com>
2023-03-28 19:42:02 -0700

    Add files via upload

---
+++ src/test/java/test.java
@@ -0,0 +1,20 @@
+package test.java;
+
+import main.java.Main;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class test {
+
+    @Test
+    public void testMain(){
+        PrintStream originalOut = System.out;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(bos));
+        Main.main(null);
+        Assert.assertEquals("Hello World", bos.toString());
+        System.setOut(originalOut);
+    }
+    }
+
+}
